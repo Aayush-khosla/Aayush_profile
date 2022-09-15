7ab11c34f6002c74e0c44fc2cd69449b9541ea18
Aayush-khosla <aayush1297.be21@chitkarauniversity.edu.in>
2022-09-15 14:28:14 +0530

    changed

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <div class="flex flex-col text-center w-full mb-20">
           <h2 class="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">Open Source</h2>
           <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">AAYUSH BRO</h1>
-          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+          <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven&apos;t heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
         </div>
         <div class="flex flex-wrap">
           <div class="xl:w-1/4 lg:w-1/2 md:w-full px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
