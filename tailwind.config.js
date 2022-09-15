f19cb4b6f2446b3ff6c929af9bf301c48fa005f7
Aayush-khosla <aayush1297.be21@chitkarauniversity.edu.in>
2022-09-15 15:13:13 +0530

    g

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // ▼ here you need to add all paths according to your needs
-    purge: ['./pages/*/.{js,ts,jsx,tsx}', './Components/*/.{js,ts,jsx,tsx}', './your-other-component-folder/*/.{js,ts,jsx,tsx}'], 
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
