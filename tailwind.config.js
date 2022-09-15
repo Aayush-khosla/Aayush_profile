44d8c8b8d4ea3b41f4a4e916df8b0333fb973ded
Aayush-khosla <aayush1297.be21@chitkarauniversity.edu.in>
2022-09-15 14:49:45 +0530

    helloBhaiThqGyAhU

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+  purge: [
+   // "./src/**/*.html",
+   // "./src/**/*.vue,
+   // "./src/**/*.jsx"
   ],
-  theme: {
-    extend: {},
-  },
+  theme: {},
+  variants: {},
   plugins: [],
 }
